40e5b8d07825884ed17fb1e24c2f23a0872928cb
satvikakolisetty <60738035+satvikakolisetty@users.noreply.github.com>
2020-05-29 17:18:25 +0530

    Update upgrade.py

--- DL/upgrade.py
+++ DL/upgrade.py
@@ -1,6 +1,6 @@
 import os
 
-acc = os.popen("cat /var/lib/jenkins/workspace/mlops_job1/accuracy.txt")
+acc = os.popen("cat /var/lib/jenkins/workspace/j1/accuracy.txt")
 acc1 = acc.read()
 print(acc1)
 acc2 = acc1.rstrip()
@@ -9,7 +9,7 @@ acc3 = float(acc2)
 
 
 if acc3<80:
-    x = os.popen("cat /var/lib/jenkins/workspace/mlops_job1/train.py | grep model.add | wc -l")
+    x = os.popen("cat /var/lib/jenkins/workspace/j1/train.py | grep model.add | wc -l")
     x1 = x.read()
     x2 = x1.rstrip()
     x3 = int(x2)
@@ -21,17 +21,17 @@ if acc3<80:
     elif x3==4:
         y = 'model.add(Dense(units=8, activation=\"relu\"))'
     else:
-        print("MAAF KARDO")
+        print("sorry:( try again")
         exit()
-    os.system("sed -i '/softmax/ i {}' /var/lib/jenkins/workspace/mlops_job1/train.py".format(y))
-    os.system("curl -u admin:redhat http://192.168.99.102:8080/view/Ml%20+%20Jenkins/job/mlops_job2/build?token=trainjob")
-    acc = os.popen("cat /var/lib/jenkins/workspace/mlops_job1/accuracy.txt")
+    os.system("sed -i '/softmax/ i {}' /var/lib/jenkins/workspace/j1/train.py".format(y))
+    os.system("curl -u root:redhat http://192.168.56.102:8080/view/Ml%20+%20Jenkins/job/j2/build?token=satvi")
+    acc = os.popen("cat /var/lib/jenkins/workspace/j1/accuracy.txt")
     acc1 = acc.read()
     print(acc1)
     acc2 = acc1.rstrip()
     print(acc2)
     acc3 = float(acc2)
 else:
-    os.system("curl -u admin:redhat 192.168.99.102:8080/view/Ml%20+%20Jenkins/job/mlops_mail/build?token=mailing")
+    
     print("ACCURACY ABOVE 80")
 
